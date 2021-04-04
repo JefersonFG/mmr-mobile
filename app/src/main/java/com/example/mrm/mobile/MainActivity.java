eecd21a8be1cea1b59db0f9c8bae7dd08fc656a1
jefersonfg <jefersonfguimaraes@gmail.com>
2021-04-04 18:53:34 -0300

    Added button for opening the camera view to scan the QR Code

--- app/src/main/java/com/example/mrm/mobile/MainActivity.java
+++ app/src/main/java/com/example/mrm/mobile/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Floating action button that leads to the camera view for scanning a QR Code
+        FloatingActionButton fab = findViewById(R.id.cameraFAB);
+        fab.setOnClickListener(view -> Snackbar.make(view, "Button clicked!", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
     }
 }
