470adcb0929fc045df05e338fc2ea06d0bbaa230
jefersonfg <jefersonfguimaraes@gmail.com>
2021-05-16 20:53:59 -0300

    Changed from rented to with customer on machine status, accept alphanumeric machine codes

--- app/src/main/java/com/example/mrm/mobile/EquipmentInfoActivity.java
+++ app/src/main/java/com/example/mrm/mobile/EquipmentInfoActivity.java
@@ -81,8 +81,8 @@ public class EquipmentInfoActivity extends AppCompatActivity
                 return getResources().getString(R.string.machineStatusMaintenance);
             case "READY_FOR_RENTAL":
                 return getResources().getString(R.string.machineStatusReadyForRental);
-            case "RENTED":
-                return getResources().getString(R.string.machineStatusRented);
+            case "CUSTOMER":
+                return getResources().getString(R.string.machineStatusCustomer);
             default:
                 // TODO: Improve error handling
                 return "";
